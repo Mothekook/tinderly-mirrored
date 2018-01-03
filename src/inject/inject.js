00d9e527a4231f8454995158d80b90f16237ff46
hungweiwu <wuxx1045@umn.edu>
2018-01-03 15:56:13 -0600

    adding jquery and got the clicking done

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,61 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+// recsGamepad__button--like
+// recsGamepad__button--dislike
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+// local storage stuff
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  for (key in changes) {
+    var storageChange = changes[key];
+    console.log(
+      'Storage key "%s" in namespace "%s" changed. ' +
+        'Old value was "%s", new value is "%s".',
+      key,
+      namespace,
+      storageChange.oldValue,
+      storageChange.newValue
+    );
+  }
+});
 
-	}
-	}, 10);
+chrome.runtime.sendMessage({}, function(response) {
+  var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
+      console.log("Hello. This message was sent from scripts/inject.js");
+
+      // add the keyboard listener for cmd shift l
+      document.addEventListener("keydown", function(event) {
+        if (event.metaKey && event.shiftKey && event.keyCode == 76) {
+          console.log("cmd shift l was pressed");
+          // set the state
+          chrome.storage.local.get(["clicked", "clickInterval"], function(
+            items
+          ) {
+            var currentClickedStatus = !items["clicked"];
+            console.log("currentClickedStatus", currentClickedStatus);
+            var clickInterval = null;
+            if (currentClickedStatus) {
+              clickInterval = setInterval(
+                // () => $(".recsGamepad__button--like").click(),
+                () => console.log("clickingggg"),
+                100
+              );
+            } else {
+              clearInterval(items["clickInterval"]);
+            }
+
+            chrome.storage.local.set(
+              {
+                clicked: currentClickedStatus,
+                clickInterval
+              },
+              function() {
+                // Notify that we saved.
+                console.log("updated");
+              }
+            );
+          });
+        }
+      });
+    }
+  }, 10);
 });
