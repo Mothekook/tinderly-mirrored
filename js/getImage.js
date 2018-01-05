2ae43c030cc8433a0855a64c7755dfcc07807ece
hungweiwu <wuxx1045@umn.edu>
2018-01-05 12:31:46 -0600

    getting image url from local storage

--- js/getImage.js
+++ js/getImage.js
@@ -1,27 +1,26 @@
-var blob = null;
-var xhr = new XMLHttpRequest();
-console.log("getting image");
-xhr.open(
-  "GET",
-  "https://images-ssl.gotinder.com/5a41e435199b629412db247d/1080x1080_cfd5fc4e-172c-4653-8394-419576f5caae.jpg"
-);
-xhr.responseType = "blob"; //force the HTTP response, response-type header to be blob
-xhr.onload = function() {
-  var reader = new FileReader();
-  reader.onloadend = function() {
-    chrome.storage.local.set(
-      {
-        image: reader.result
-      },
-      function() {
-        // Notify that we saved.
-        console.log("saved image");
-      }
-    );
-  };
-  reader.readAsDataURL(xhr.response);
-};
-xhr.send();
+chrome.storage.local.get("imageUrl", function(items) {
+  if (!items["imageUrl"]) {
+    return;
+  }
+  var xhr = new XMLHttpRequest();
+  console.log("getting image");
+  xhr.open("GET", items["imageUrl"]);
+  xhr.responseType = "blob"; //force the HTTP response, response-type header to be blob
 
-//https://ourcodeworld.com/articles/read/322/how-to-convert-a-base64-image-into-a-image-file-and-upload-it-with-an-asynchronous-form-using-jquery
-//base 64 to blob to upload
+  xhr.onload = function() {
+    var reader = new FileReader();
+    reader.onloadend = function() {
+      chrome.storage.local.set(
+        {
+          image: reader.result
+        },
+        function() {
+          // Notify that we saved.
+          console.log("saved image");
+        }
+      );
+    };
+    reader.readAsDataURL(xhr.response);
+  };
+  xhr.send();
+});
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,50 +2,97 @@
 // recsGamepad__button--dislike
 // recCard__img
 
-chrome.runtime.sendMessage({}, function(response) {
-  var readyStateCheckInterval = setInterval(function() {
-    if (document.readyState === "complete") {
-      clearInterval(readyStateCheckInterval);
-      console.log("Hello. This message was sent from scripts/inject.js");
+// helper function to convert b64 string to Blob to send to Haystack
+function b64toBlob(b64Data, contentType, sliceSize) {
+  contentType = contentType || "";
+  sliceSize = sliceSize || 512;
 
-      // add the keyboard listener for cmd shift l
-      document.addEventListener("keydown", function(event) {
-        if (event.metaKey && event.shiftKey && event.keyCode == 76) {
-          console.log("cmd shift l was pressed");
-          // set the state
-          chrome.storage.local.get(
-            ["clicked", "clickInterval", "image"],
-            function(items) {
-              console.log(items);
+  var byteCharacters = atob(b64Data);
+  var byteArrays = [];
 
-              var currentClickedStatus = !items["clicked"];
-              console.log("currentClickedStatus", currentClickedStatus);
+  for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    var slice = byteCharacters.slice(offset, offset + sliceSize);
 
-              var clickInterval = null;
-              if (currentClickedStatus) {
-                clickInterval = setInterval(
-                  // () => $(".recsGamepad__button--like").click(),
-                  () => console.log("clickingggg"),
-                  100
-                );
-              } else {
-                clearInterval(items["clickInterval"]);
+    var byteNumbers = new Array(slice.length);
+    for (var i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+
+    var byteArray = new Uint8Array(byteNumbers);
+
+    byteArrays.push(byteArray);
+  }
+
+  var blob = new Blob(byteArrays, { type: contentType });
+  return blob;
+}
+
+function getContentType(b64String) {
+  var block = b64String.split(";");
+  return block[0].split(":")[1]; // In this case "image/gif"
+}
+
+function getRealData(b64String) {
+  var block = b64String.split(";");
+  return block[1].split(",")[1]; // In this case "R0lGODlhPQBEAPeoAJosM...."
+}
+
+function getImageUrl() {
+  return $(".recCard__img")
+    .css("background-image")
+    .replace(/^url\(["']?/, "")
+    .replace(/["']?\)$/, "");
+}
+
+// the the document is ready then we inject
+var readyStateCheckInterval = setInterval(function() {
+  if (document.readyState === "complete") {
+    clearInterval(readyStateCheckInterval);
+    console.log("Hello. This message was sent from scripts/inject.js");
+
+    // add the keyboard listener for cmd shift l
+    document.addEventListener("keydown", function(event) {
+      if (event.metaKey && event.shiftKey && event.keyCode == 76) {
+        console.log("cmd shift l was pressed");
+        // set the state
+        chrome.storage.local.get(["clicked", "clickInterval"], function(items) {
+          console.log(items);
+
+          var currentClickedStatus = !items["clicked"];
+          console.log("currentClickedStatus", currentClickedStatus);
+
+          var clickInterval = null;
+          if (currentClickedStatus) {
+            clickInterval = setInterval(
+              // () => $(".recsGamepad__button--like").click(),
+              () => console.log("clickingggg"),
+              100
+            );
+            var imageUrl = getImageUrl();
+            chrome.runtime.local.set(
+              {
+                imageUrl
+              },
+              function() {
+                console.log("got image url");
               }
+            );
+          } else {
+            clearInterval(items["clickInterval"]);
+          }
 
-              chrome.storage.local.set(
-                {
-                  clicked: currentClickedStatus,
-                  clickInterval
-                },
-                function() {
-                  // Notify that we saved.
-                  console.log("updated");
-                }
-              );
+          chrome.storage.local.set(
+            {
+              clicked: currentClickedStatus,
+              clickInterval
+            },
+            function() {
+              // Notify that we saved.
+              console.log("updated");
             }
           );
-        }
-      });
-    }
-  }, 10);
-});
+        });
+      }
+    });
+  }
+}, 10);
